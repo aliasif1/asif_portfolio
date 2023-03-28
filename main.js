d2709b09d6249b4decc9358da4d339e6ca8c88e8
Asif Mehmuda <asif@Asifs-MacBook-Pro.local>
2023-03-28 16:34:11 -0600

    code changed

--- main.js
+++ main.js
@@ -1,17 +1,70 @@
-// moon element
-const moonElement = document.querySelector(".fa-moon");
-const sunElement = document.querySelector(".fa-sun");
+// Setting light dark theme
+let darkTheme = true;
+const themeElement = document.querySelector(".theme-icon");
+themeElement.addEventListener("click", () => {
+    // change dark theme to light theme
+    if(darkTheme){
+        darkTheme = false;
+        themeElement.classList.remove('fa-sun');
+        themeElement.classList.add('fa-moon');
+        document.body.classList.remove("dark");
+    }
+    else{
+        darkTheme = true;
+        themeElement.classList.remove('fa-moon');
+        themeElement.classList.add('fa-sun');
+        document.body.classList.add("dark");
+    }
 
-sunElement.style.display = "none";
-
-moonElement.addEventListener("click", () => {
-    moonElement.style.display = "none";
-    sunElement.style.display = "inline";
-    document.body.classList.add("dark");
 })
 
-sunElement.addEventListener("click", () => {
-    sunElement.style.display = "none";
-    moonElement.style.display = "inline";
-    document.body.classList.remove("dark");
-})
+// Setting options display
+const handleDisplay = (option) => {
+    console.log(option);
+    if(option === display){
+        return;
+    }
+    // hide the last display 
+    if(display === 'info'){
+        infoButton.classList.toggle('selected');
+        infoSection.classList.toggle('hide');
+    }
+    else if(display === 'projects'){
+        projectsButton.classList.toggle('selected');
+        projectsSection.classList.toggle('hide');
+    }
+    else if(display === 'skills'){
+        SkillsButton.classList.toggle('selected');
+        skillsSection.classList.toggle('hide');
+    }
+
+    // show the new display 
+    if(option === 'info'){
+        infoButton.classList.toggle('selected');
+        infoSection.classList.toggle('hide');
+    }
+    else if(option === 'projects'){
+        projectsButton.classList.toggle('selected');
+        projectsSection.classList.toggle('hide');
+    }
+    else if(option === 'skills'){
+        SkillsButton.classList.toggle('selected');
+        skillsSection.classList.toggle('hide');
+    }
+    display = option
+}
+
+let display = 'projects';
+const infoSection = document.getElementById('info')
+const projectsSection = document.getElementById('projects')
+const skillsSection = document.getElementById('skills')
+
+// Buttons
+const infoButton = document.getElementById('infoButton')
+const projectsButton = document.getElementById('projectsButton')
+const SkillsButton = document.getElementById('skillsButton')
+
+
+infoButton.addEventListener("click", () => handleDisplay('info'))
+projectsButton.addEventListener("click", () => handleDisplay('projects'))
+SkillsButton.addEventListener("click", () => handleDisplay('skills'))
